496a5968708166d7b72d1c3c348b9af4815b7629
IHaxU <30863082+IHaxU@users.noreply.github.com>
2022-04-30 00:46:43 +0200

    Update Shizo.lua

--- Shizo.lua
+++ Shizo.lua
@@ -1,2 +1,10 @@
---I realize this isn't much of a crack and more of a leak but yeah this is still a product listed on their website so here you go
+-- I realize this isn't much of a crack and more of a leak but yeah this is still a product listed on their website so here you go
+
+-- Games supported:
+  -- Natural Disaster
+  -- Prison Life
+  -- Laundry Simulator
+  -- Lucky Blocks
+  -- Build A Boat For Treasure
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/flix05/sohot/main/obfuscated20",true))()
