7b721239560a208bf98baf71c693eb8d42029a18
IHaxU <30863082+IHaxU@users.noreply.github.com>
2022-04-30 00:42:51 +0200

    Create Shizo.lua

---
+++ Shizo.lua
@@ -0,0 +1,2 @@
+--I realize this isn't much of a crack and more of a leak but yeah this is still a product listed on their website so here you go
+loadstring(game:HttpGet("https://raw.githubusercontent.com/flix05/sohot/main/obfuscated20",true))()
